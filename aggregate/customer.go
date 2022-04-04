215ce2f3959729c9869243160391cd6600b5a570
Novanda Ahsan <nathoilla@indo-soft.com>
2022-04-04 10:14:46 +0700

    first commit

---
+++ aggregate/customer.go
@@ -0,0 +1,58 @@
+package aggregate
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/novanda1/ddd-go/entity"
+	"github.com/novanda1/ddd-go/valueobject"
+)
+
+var (
+	ErrInvalidPerson = errors.New("a customer has to have a valid person")
+)
+
+type Customer struct {
+	person       *entity.Person
+	products     []*entity.Item
+	transactions []valueobject.Transaction
+}
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
+}
+
+func (c *Customer) GetID() uuid.UUID {
+	return c.person.ID
+}
+
+func (c *Customer) SetID(id uuid.UUID) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+	c.person.ID = id
+}
+
+func (c *Customer) SetName(name string) {
+	if c.person == nil {
+		c.person = &entity.Person{}
+	}
+	c.person.Name = name
+}
+
+func (c *Customer) GetName() string {
+	return c.person.Name
+}
